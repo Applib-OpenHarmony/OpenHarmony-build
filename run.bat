adc5a5aa98e5b4d8dea0ae12751eca8f6195cf4c
Srihari <srihariprasad.gv@gmail.com>
2022-02-23 00:09:52 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,3 @@
-gradlew assembleDebug checkstyle sonarqube --info
+IF "%LANG%"=="JAVA" REM do gradlew assembleDebug checkstyle sonarqube --info
+IF "%LANG%"=="JS" REM do echo "%LANG%
+IF "%LANG%"=="C" REM do "%LANG%
