755a66681bc109c2903d239aabd3fd166fdf0a1b
Srihari <srihariprasad.gv@gmail.com>
2022-02-28 23:59:59 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
-$ErrorActionPreference = 'SilentlyContinue'
+set $ErrorActionPreference = 'SilentlyContinue'
 echo %LANG%
 echo "executing eslint"
-.\node_modules\.bin\eslint -c SDK\config\.eslintrc.js entry -o report.html
+.\node_modules\.bin\eslint -c SDK\config\.eslintrc.js entry -o report.html 
 echo "executing gradle"
 gradlew assembleDebug sonarqube --info
