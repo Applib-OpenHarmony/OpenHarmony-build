28d0df4c67791f758ed60f0d412842f8eb458540
Srihari <srihariprasad.gv@gmail.com>
2022-02-28 23:16:37 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,13 +1,5 @@
 echo %LANG%
-$value=%LANG%
-if ( $value -is JAVA )
-{
-gradlew assembleDebug checkstyle sonarqube --info
-}
-if ( $value -is JS )
-{
 echo "executing eslint"
 .\node_modules\.bin\eslint -c ${{ github.workspace }}\SDK\config\.eslintrc.js entry -o report.html
 echo "executing gradle"
 gradlew assembleDebug sonarqube --info
-}
