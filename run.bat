871f2d3f35f07fdeb88be6fe7bc9fd36a89f4334
Srihari <srihariprasad.gv@gmail.com>
2022-02-28 23:25:33 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 echo %LANG%
 echo "executing eslint"
-.\node_modules\.bin\eslint -c ${{ github.workspace }}\SDK\config\.eslintrc.js entry -o report.html
+.\node_modules\.bin\eslint -c $GITHUB_WORKSPACE\SDK\config\.eslintrc.js entry -o report.html
 echo "executing gradle"
 gradlew assembleDebug sonarqube --info
