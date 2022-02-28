f36149b3bcf2b5d0029ffe14a5ca63b66e8ceb77
Srihari <srihariprasad.gv@gmail.com>
2022-02-28 22:55:46 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,10 @@
 echo %LANG%
 IF "%LANG%"=="JAVA" gradlew assembleDebug checkstyle sonarqube --info
-IF "%LANG%"=="JS" .\node_modules\.bin\eslint -c ${{ github.workspace }}\SDK\config\.eslintrc.js entry -o report.html; gradlew assembleDebug sonarqube --info
+IF "%LANG%"=="JS" 
+{
+echo "executing eslint"
+.\node_modules\.bin\eslint -c ${{ github.workspace }}\SDK\config\.eslintrc.js entry -o report.html
+echo "executing gradle"
+gradlew assembleDebug sonarqube --info
+}
 IF "%LANG%"=="C" REM do "%LANG%
