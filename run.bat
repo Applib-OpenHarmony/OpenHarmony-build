34ce61b55846c0662785d1b8dd2a8b887c72b1bc
Srihari <srihariprasad.gv@gmail.com>
2022-03-15 19:34:47 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 set $ErrorActionPreference = 'SilentlyContinue'
 echo %LANG%
 echo "executing eslint"
-.\node_modules\.bin\eslint --no-eslintrc --ext .js,.jsx,.ts,.tsx entry -o report.html --debug 
+.\node_modules\.bin\eslint -c ${{ github.workspace }}\SDK\config\.eslintrc.js --format json entry --output-file result.json
 echo "executing gradle"
 gradlew assembleDebug sonarqube --info
